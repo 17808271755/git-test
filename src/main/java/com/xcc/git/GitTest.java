8af877830449d881fb6fcbcdfedf0b1ac0453392
SJF <378464640@qq.com>
2021-08-02 16:58:01 +0800

    hot-fix commit

--- src/main/java/com/xcc/git/GitTest.java
+++ src/main/java/com/xcc/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello,xcc的Git");
         System.out.println("hello,xcc的Git2");
         System.out.println("hello,xcc的Git3");
+        System.out.println("hello,xcc的Git4");
     }
 }
