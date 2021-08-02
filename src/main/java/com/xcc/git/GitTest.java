5cb4dca2b5fc3abf23f325f534b0cae06013d7b1
SJF <378464640@qq.com>
2021-08-02 14:43:59 +0800

    IDEA first commit

---
+++ src/main/java/com/xcc/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.xcc.git;
+
+/**
+ * GitTest
+ *
+ * @author <sjf753@qq.com>
+ * @since 2021/8/2 14:41
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,xcc的Git");
+    }
+}
